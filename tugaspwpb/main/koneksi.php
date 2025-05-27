0143784d6cd27da5fd00813c84a2cc52cd7cec89
KateKateline <rizkyramadhan12098@gmail.com>
2025-05-27 11:46:01 -0500

    Delete tugaspwpb directory

--- tugaspwpb/main/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $koneksi = mysqli_connect("localhost", "root", "", "webi");
-
-    if(mysqli_connect_error()){
-        echo "Koneksi ke database tidak berjalan",mysqli_connect_error();
-    }
-?>
